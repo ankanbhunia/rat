784131db7bd667dc08793dee72cc6c1c7bb25c30
Ankan Bhunia <ankankumarbhunia@gmail.com>
2025-07-30 13:08:14 +0100

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -8,6 +8,17 @@ echo "Navigating to: $RAT_DIR_ABS_PATH"
 cd "$RAT_DIR_ABS_PATH" || { echo "Error: Could not navigate to $RAT_DIR_ABS_PATH. Aborting upgrade."; exit 1; }
 
 # Perform git pull
+read -p "Local changes detected. Do you want to stash all changes before pulling? (y/n): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Stashing all local changes..."
+    git stash --all
+else
+    echo "Aborting upgrade. Please commit or stash your changes manually."
+    exit 1
+fi
+
 echo "Pulling latest changes from git repository..."
 git pull
 
